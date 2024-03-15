e6e5bd1c67479f40a75f4a209c2fa78514a48f70
koraygurses <koraygurses96@gmail.com>
2024-03-15 10:40:30 +0300

    Updates

--- assets/ios.transition-legacy-15e3672a.js
+++ assets/ios.transition-legacy-15e3672a.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-cfab2000.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-badf5756.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
