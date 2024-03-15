86e771b294ad3e23be207a2e1f70106e13c020e5
koraygurses <koraygurses96@gmail.com>
2024-03-15 10:14:17 +0300

    Updates

--- assets/md.transition-legacy-99046834.js
+++ assets/md.transition-legacy-99046834.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-badf5756.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-ccf5e5ef.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
--- assets/swipe-back-legacy-be4e1181.js
+++ assets/swipe-back-legacy-be4e1181.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-badf5756.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-ccf5e5ef.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
